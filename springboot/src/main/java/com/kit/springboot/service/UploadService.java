3375c4ef60c51afa71780c74189f331aa66bad60
wangheng <1012056369@qq.com>
2020-09-23 17:33:35 +0800

    springcloud

---
+++ springboot/src/main/java/com/kit/springboot/service/UploadService.java
@@ -0,0 +1,11 @@
+package com.kit.springboot.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author wangheng
+ * @date 2020/8/19
+ */
+@Service
+public class UploadService {
+}
